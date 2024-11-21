92b80ae207830a425e4faee7d4a35c6e2d9fd664
Paul Lightman <pavel.kulyasov@gmail.com>
2024-11-21 03:05:43 +0300

    fix: refactor js code

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,60 +1,80 @@
-var data = [
+var positionSystemsData = [
     {
-        "title": "Система навигации «Локус»",
-        "desc": "Для создания летной области необходимо четыре ультразвуковых излучателя и специальный модуль на самом аппарате.",
-        "link": "https://geoscan.ru",
-        "img": 'assets/images/preview/locus-nav.png'
+        'title': 'Система навигации «Локус»',
+        'desc': 'Для создания летной области необходимо четыре ультразвуковых излучателя и специальный модуль на самом аппарате.',
+        'link': 'https://geoscan.ru/',
+        'img': 'assets/images/preview/locus-nav.png',
     },
     {
-        "title": "Система ИК-навигации",
-        "desc": "Дополнительная плата на коптере и базовая станция поддерживают зону полета 12 кв. м.",
-        "link": "https://geoscan.ru",
-        "img": 'assets/images/preview/ir-nav.png'
+        'title': 'Система ИК-навигации',
+        'desc': 'Дополнительная плата на коптере и базовая станция поддерживают зону полета 12 кв. м.',
+        'link': 'https://geoscan.ru/',
+        'img': 'assets/images/preview/ir-nav.png',
     },
     {
-        "title": "Оптическая система навигации",
-        "desc": "Идет в базовой комплектации. Позиционирование происходит благодаря оптическому датчику, отслеживающему смещение под Пионером.",
-        "link": "https://geoscan.ru",
-        "img": 'assets/images/preview/optical-nav.png'
+        'title': 'Оптическая система навигации',
+        'desc': 'Идет в базовой комплектации. Позиционирование происходит благодаря оптическому датчику, отслеживающему смещение под Пионером.',
+        'link': 'https://geoscan.ru/',
+        'img': 'assets/images/preview/optical-nav.png',
     },
     {
-        "title": "GPS-система навигации",
-        "desc": "Предназначена для пилотирования коптером на открытом воздухе. Для этого на главную плату автопилота нужно установить дополнительный модуль.",
-        "link": "https://geoscan.ru",
-        "img": 'assets/images/preview/gps-nav.png'
-    }
+        'title': 'GPS-система навигации',
+        'desc': 'Предназначена для пилотирования коптером на открытом воздухе. Для этого на главную плату автопилота нужно установить дополнительный модуль.',
+        'link': 'https://geoscan.ru/',
+        'img': 'assets/images/preview/gps-nav.png',
+    },
 ];
-var previewImg = document.querySelector('.content__image');
-var contentControls = document.querySelector('.content__controls');
-var contentLink = document.querySelector('.content__link');
-var CURRENT = 0;
-var BUTTONS = [];
-var changeImage = function (idx) {
-    previewImg.classList.add('content__image-fadeout');
-    setTimeout(function () {
-        previewImg.setAttribute('src', data[idx].img);
-        previewImg.setAttribute('alt', data[idx].title);
-        previewImg.classList.remove('content__image-fadeout');
-        previewImg.classList.add('content__image-fadein');
-    }, 400);
-};
-var changeButton = function (index) {
-    BUTTONS[CURRENT].classList.toggle('content__button-active');
-    BUTTONS[index].classList.toggle('content__button-active');
-    CURRENT = index;
-};
-var renderButtons = function () {
-    data.forEach(function (data, idx) {
-        contentControls.insertAdjacentHTML('beforeend', "<button class='content__button ".concat(!idx ? 'content__button-active' : '', "'><span>").concat(data.title, "</span></buton>"));
-        contentLink.setAttribute('href', data.link);
-    });
-    BUTTONS = document.querySelectorAll('.content__controls button');
-    BUTTONS.forEach(function (button, idx) { return button.addEventListener('click', function (event) {
-        changeButton(idx);
-        changeImage(idx);
-    }); });
-};
-window.onload = function () {
-    previewImg.setAttribute('src', data[0].img);
-    renderButtons();
-};
+var PositionSystemsContent = /** @class */ (function () {
+    function PositionSystemsContent(data) {
+        this.CURRENT = 0;
+        this.previewImage = document.querySelector('.content__image');
+        this.controlsContainer = document.querySelector('.content__controls');
+        this.detailsLink = document.querySelector('.content__link');
+        this.buttonsList = [];
+        if (!Array.isArray(data) || !data.length) {
+            throw new Error('Data not provided');
+        }
+        this.data = data;
+    }
+    PositionSystemsContent.prototype.changeImage = function (index) {
+        var _this = this;
+        this.previewImage.classList.add('content__image-fadeout');
+        setTimeout(function () {
+            _this.previewImage.setAttribute('src', _this.data[index].img);
+            _this.previewImage.setAttribute('alt', _this.data[index].title);
+            _this.previewImage.classList.remove('content__image-fadeout');
+            _this.previewImage.classList.add('content__image-fadein');
+        }, 400);
+    };
+    PositionSystemsContent.prototype.changeButton = function (index) {
+        this.buttonsList[this.CURRENT].classList.toggle('content__button-active');
+        this.buttonsList[index].classList.toggle('content__button-active');
+        this.detailsLink.setAttribute('href', this.data[index].link);
+        this.CURRENT = index;
+    };
+    PositionSystemsContent.prototype.renderButtons = function () {
+        var _this = this;
+        this.data.forEach(function (data, idx) {
+            _this.controlsContainer.insertAdjacentHTML('beforeend', "<button class='content__button ".concat(!idx ? 'content__button-active' : '', "'>\n\t\t\t\t\t\t<span>").concat(data.title, "</span>\n\t\t\t\t</buton>"));
+            _this.detailsLink.setAttribute('href', data.link);
+        });
+        this.buttonsList = document.querySelectorAll('.content__controls button');
+        this.buttonsList.forEach(function (button, index) { return button.addEventListener('click', function (event) {
+            if (index === _this.CURRENT)
+                return;
+            _this.changeButton(index);
+            _this.changeImage(index);
+        }); });
+    };
+    PositionSystemsContent.prototype.init = function () {
+        if (!this.previewImage || !this.controlsContainer || !this.detailsLink) {
+            throw new Error('DOM elements not found');
+        }
+        this.previewImage.setAttribute('src', this.data[0].img);
+        this.previewImage.setAttribute('alt', this.data[0].title);
+        this.renderButtons();
+    };
+    return PositionSystemsContent;
+}());
+var positionSystemContent = new PositionSystemsContent(positionSystemsData);
+window.onload = function () { return positionSystemContent.init(); };
--- assets/scripts/script.ts
+++ assets/scripts/script.ts
@@ -5,74 +5,96 @@ interface IData {
 	img: string
 }
 
-const data: IData[] = [
+const positionSystemsData: IData[] = [
 	{
-		"title": "Система навигации «Локус»",
-		"desc": "Для создания летной области необходимо четыре ультразвуковых излучателя и специальный модуль на самом аппарате.",
-		"link": "https://geoscan.ru",
-		"img": 'assets/images/preview/locus-nav.png'
+		'title': 'Система навигации «Локус»',
+		'desc': 'Для создания летной области необходимо четыре ультразвуковых излучателя и специальный модуль на самом аппарате.',
+		'link': 'https://geoscan.ru/',
+		'img': 'assets/images/preview/locus-nav.png',
 	},
 	{
-		"title": "Система ИК-навигации",
-		"desc": "Дополнительная плата на коптере и базовая станция поддерживают зону полета 12 кв. м.",
-		"link": "https://geoscan.ru",
-		"img": 'assets/images/preview/ir-nav.png'
+		'title': 'Система ИК-навигации',
+		'desc': 'Дополнительная плата на коптере и базовая станция поддерживают зону полета 12 кв. м.',
+		'link': 'https://geoscan.ru/',
+		'img': 'assets/images/preview/ir-nav.png',
 	},
 	{
-		"title": "Оптическая система навигации",
-		"desc": "Идет в базовой комплектации. Позиционирование происходит благодаря оптическому датчику, отслеживающему смещение под Пионером.",
-		"link": "https://geoscan.ru",
-		"img": 'assets/images/preview/optical-nav.png'
+		'title': 'Оптическая система навигации',
+		'desc': 'Идет в базовой комплектации. Позиционирование происходит благодаря оптическому датчику, отслеживающему смещение под Пионером.',
+		'link': 'https://geoscan.ru/',
+		'img': 'assets/images/preview/optical-nav.png',
 	},
 	{
-		"title": "GPS-система навигации",
-		"desc": "Предназначена для пилотирования коптером на открытом воздухе. Для этого на главную плату автопилота нужно установить дополнительный модуль.",
-		"link": "https://geoscan.ru",
-		"img": 'assets/images/preview/gps-nav.png'
-	}
+		'title': 'GPS-система навигации',
+		'desc': 'Предназначена для пилотирования коптером на открытом воздухе. Для этого на главную плату автопилота нужно установить дополнительный модуль.',
+		'link': 'https://geoscan.ru/',
+		'img': 'assets/images/preview/gps-nav.png',
+	},
 ]
 
-const previewImg = document.querySelector('.content__image')
-const contentControls = document.querySelector('.content__controls')
-const contentLink = document.querySelector('.content__link')
+
+class PositionSystemsContent {
+	private data: IData[]
+	private CURRENT: number = 0
+	private previewImage: HTMLImageElement | null = document.querySelector('.content__image')
+	private controlsContainer: HTMLDivElement | null = document.querySelector('.content__controls')
+	private detailsLink: HTMLAnchorElement | null = document.querySelector('.content__link')
+	private buttonsList: NodeListOf<HTMLButtonElement> | undefined[] = []
+
+	constructor(data: IData[]) {
+		if (!Array.isArray(data) || !data.length) {
+			throw new Error('Data not provided')
+		}
+		this.data = data
+	}
+
+	changeImage(index: number) {
+		this.previewImage.classList.add('content__image-fadeout')
+
+		setTimeout(() => {
+			this.previewImage.setAttribute('src', this.data[index].img)
+			this.previewImage.setAttribute('alt', this.data[index].title)
+			this.previewImage.classList.remove('content__image-fadeout')
+			this.previewImage.classList.add('content__image-fadein')
+		}, 400)
+	}
+
+	private changeButton(index: number) {
+		this.buttonsList[this.CURRENT].classList.toggle('content__button-active')
+		this.buttonsList[index].classList.toggle('content__button-active')
+		this.detailsLink.setAttribute('href', this.data[index].link)
+		this.CURRENT = index
+	}
+
+	private renderButtons() {
+		this.data.forEach((data, idx) => {
+			this.controlsContainer.insertAdjacentHTML('beforeend',
+				`<button class='content__button ${!idx ? 'content__button-active' : ''}'>
+						<span>${data.title}</span>
+				</buton>`)
+			this.detailsLink.setAttribute('href', data.link)
+		})
+
+		this.buttonsList = document.querySelectorAll('.content__controls button')
+
+		this.buttonsList.forEach((button, index) => button.addEventListener('click', (event) => {
+			if (index === this.CURRENT) return
+			this.changeButton(index)
+			this.changeImage(index)
+		}))
+	}
 
 
-let CURRENT = 0
-let BUTTONS: NodeListOf<HTMLButtonElement> | undefined[] = []
+	init() {
+		if (!this.previewImage || !this.controlsContainer || !this.detailsLink) {
+			throw new Error('DOM elements not found')
+		}
 
-
-const changeImage = (idx: number) => {
-	previewImg.classList.add('content__image-fadeout')
-
-	setTimeout(() => {
-		previewImg.setAttribute('src', data[idx].img)
-		previewImg.setAttribute('alt', data[idx].title)
-		previewImg.classList.remove('content__image-fadeout')
-		previewImg.classList.add('content__image-fadein')
-	}, 400)
+		this.previewImage.setAttribute('src', this.data[0].img)
+		this.previewImage.setAttribute('alt', this.data[0].title)
+		this.renderButtons()
+	}
 }
 
-const changeButton = (index: number) => {
-	BUTTONS[CURRENT].classList.toggle('content__button-active')
-	BUTTONS[index].classList.toggle('content__button-active')
-	CURRENT = index
-}
-
-const renderButtons = () => {
-	data.forEach((data,idx) => {
-		contentControls.insertAdjacentHTML('beforeend',`<button class='content__button ${!idx ? 'content__button-active' : ''}'><span>${data.title}</span></buton>`)
-		contentLink.setAttribute('href', data.link)
-	})
-
-	BUTTONS = document.querySelectorAll('.content__controls button')
-
-	BUTTONS.forEach((button,idx) => button.addEventListener('click', (event) => {
-		changeButton(idx)
-		changeImage(idx)
-	}))
-}
-
-window.onload = () => {
-	previewImg.setAttribute('src', data[0].img)
-	renderButtons()
-}
+const positionSystemContent = new PositionSystemsContent(positionSystemsData)
+window.onload = () => positionSystemContent.init()
