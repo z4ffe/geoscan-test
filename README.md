04a9dfd13de169ba1391bad50258322b448ad4de
Paul Lightman <pavel.kulyasov@gmail.com>
2024-11-21 15:27:31 +0300

    add: readme, fix: responsive etc

---
+++ README.md
@@ -0,0 +1,6 @@
+<h2 align='center'>Test Task by GS</h2>
+<hr>
+<div align='center'><b>Pure HTML, TS and SCSS</b></div>
+<hr>
+<div align='center'>
+</div>
--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -2,25 +2,25 @@ const positionSystemsData = [
     {
         'title': 'Система навигации «Локус»',
         'desc': 'Для создания летной области необходимо четыре ультразвуковых излучателя и специальный модуль на самом аппарате.',
-        'link': 'https://geoscan.ru/',
+        'link': 'https://google.com/',
         'img': 'assets/images/preview/locus-nav.png',
     },
     {
         'title': 'Система ИК-навигации',
         'desc': 'Дополнительная плата на коптере и базовая станция поддерживают зону полета 12 кв. м.',
-        'link': 'https://geoscan.ru/',
+        'link': 'https://yahoo.com/',
         'img': 'assets/images/preview/ir-nav.png',
     },
     {
         'title': 'Оптическая система навигации',
         'desc': 'Идет в базовой комплектации. Позиционирование происходит благодаря оптическому датчику, отслеживающему смещение под Пионером.',
-        'link': 'https://geoscan.ru/',
+        'link': 'https://yandex.com/',
         'img': 'assets/images/preview/optical-nav.png',
     },
     {
         'title': 'GPS-система навигации',
         'desc': 'Предназначена для пилотирования коптером на открытом воздухе. Для этого на главную плату автопилота нужно установить дополнительный модуль.',
-        'link': 'https://geoscan.ru/',
+        'link': 'https://bing.com/',
         'img': 'assets/images/preview/gps-nav.png',
     },
 ];
